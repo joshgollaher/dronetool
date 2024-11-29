afeafc368743eb2714f6e38563177158dab9a784
Joshua Gollaher <joshg@mopine.com>
2024-11-28 21:43:32 -0600

    Removed old comment

--- src/drones/SimpleDrone.cpp
+++ src/drones/SimpleDrone.cpp
@@ -22,7 +22,7 @@ namespace DroneTool
         m_battery = new Battery(3150);
 
         constexpr double max_rotor_rpm = 14000;
-        const Distribution rpm_thrust_curve(0, max_rotor_rpm, generate_rpm_thrust_curve(max_rotor_rpm, 100)); // Fill in the vector ({}) with the thrust values. Assume they will be evenly spaced, and a log-style curve
+        const Distribution rpm_thrust_curve(0, max_rotor_rpm, generate_rpm_thrust_curve(max_rotor_rpm, 100));
         const Distribution rpm_power_draw_curve(0, max_rotor_rpm, {0, 45}); // Linear curve for now
         m_rotor_1 = new Rotor(max_rotor_rpm, rpm_thrust_curve, rpm_power_draw_curve);
         m_rotor_2 = new Rotor(max_rotor_rpm, rpm_thrust_curve, rpm_power_draw_curve);
